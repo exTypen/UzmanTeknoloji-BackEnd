240341d797bf2a371cc31c368fda422fa01b2ccc
Yasir KILINÇ <63153220+exTypen@users.noreply.github.com>
2021-05-12 18:01:19 +0300

    simple edits

--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -1,9 +1,10 @@
 ﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfBrandDal : EfEntityRepositoryBase<Brand, UzmanContext>
+    public class EfBrandDal : EfEntityRepositoryBase<Brand, UzmanContext>, IBrandDal
     {
         
     }
